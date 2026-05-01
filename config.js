28ee41bbac39b18ab8997ce9d621b127dc33d7d8
Mr. Nima Streaming <113153450+nima391@users.noreply.github.com>
2026-05-01 15:49:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ynITzSAB#2FigJio_-lMDhEIIWo5n_uOX-8djumpNyiX5HW2uwJw",
+SESSION_ID: process.env.SESSION_ID || "C6IhnRgQ#uxqOt9yI_G_ZpUs2YtfAOwonw7NzWvgBgEkfpW_HIgo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/nima391/NimA-s-Bot/blob/main/images/NimA%20Functional%20Bot%20Logo.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 NimA's Bot Is Alive Now😍*",
 BOT_OWNER: '94773918295',  // Replace with the owner's phone number
